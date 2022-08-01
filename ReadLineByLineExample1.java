38a6dd97d697dc6d289cdde73cd922c85596225f
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-08-01 20:58:26 +0530

    Add files via upload

---
+++ ReadLineByLineExample1.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.io.*; 
+public class ReadLineByLineExample1 {
+    public static void main(String args[])  
+{  
+try  
+{  
+File file=new File("Demo.txt");    //creates a new file instance  
+FileReader fr=new FileReader(file);   //reads the file  
+BufferedReader br=new BufferedReader(fr);  //creates a buffering character input stream  
+StringBuffer sb=new StringBuffer();    //constructs a string buffer with no characters  
+String line;  
+while((line=br.readLine())!=null)  
+{  
+sb.append(line);      //appends line to string buffer  
+sb.append("\n");     //line feed   
+}  
+fr.close();    //closes the stream and release the resources  
+System.out.println("Contents of File: ");  
+System.out.println(sb.toString());   //returns a string that textually represents the object  
+}  
+catch(IOException e)  
+    {  
+e.printStackTrace();  
+}  
+}  
+}  
+
